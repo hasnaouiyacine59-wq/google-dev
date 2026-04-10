71f83f11d537114a2ed88b573dd69f76d4815d1d
oo33 <hasnaouiyacine59@gmail.com>
2026-04-10 14:13:33 +0100

    Fix dbus: use dbus-run-session to wrap XFCE session

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Start D-Bus
-eval $(dbus-launch --sh-syntax)
-export DBUS_SESSION_BUS_ADDRESS
-
 # Start virtual display
 Xvfb $DISPLAY -screen 0 $RESOLUTION &
 sleep 1
@@ -12,32 +8,33 @@ sleep 1
 # Fix NumLock on
 numlockx on
 
-# Start XFCE components individually (more reliable in headless containers)
-xfwm4 --daemon --sm-client-disable
-xfdesktop --sm-client-disable &
-xfce4-panel --sm-client-disable &
-xfsettingsd --sm-client-disable &
-sleep 2
+# Run entire session under dbus
+exec dbus-run-session -- bash -c '
+    # Start XFCE components
+    xfwm4 --daemon --sm-client-disable
+    xfdesktop --sm-client-disable &
+    xfce4-panel --sm-client-disable &
+    xfsettingsd --sm-client-disable &
+    sleep 2
 
-# Apply Arc theme + icons
-xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark" --create -t string
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Adwaita" --create -t string
-xfconf-query -c xfwm4 -p /general/theme -s "Arc-Dark" --create -t string
+    # Apply Arc-Dark theme
+    xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark" --create -t string
+    xfconf-query -c xsettings -p /Net/IconThemeName -s "Adwaita" --create -t string
+    xfconf-query -c xfwm4 -p /general/theme -s "Arc-Dark" --create -t string
 
-# Set desktop background to solid dark color (no black void)
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/color-style -s 0 --create -t int
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/rgba1 \
-    --create -t double -t double -t double -t double -s 0.17 -s 0.17 -s 0.17 -s 1.0
+    # Desktop background solid dark grey
+    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/color-style -s 0 --create -t int
+    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/rgba1 \
+        --create -t double -t double -t double -t double -s 0.17 -s 0.17 -s 0.17 -s 1.0
 
-# Clipboard sync for noVNC copy/paste
-autocutsel -fork
-autocutsel -selection PRIMARY -fork
+    # Clipboard sync for noVNC
+    autocutsel -fork
+    autocutsel -selection PRIMARY -fork
 
-# Start VNC server
-x11vnc -display $DISPLAY -nopw -forever -shared -rfbport 5900 -noxdamage &
-sleep 1
+    # VNC + noVNC
+    x11vnc -display $DISPLAY -nopw -forever -shared -rfbport 5900 -noxdamage &
+    sleep 1
+    websockify --web /usr/share/novnc/ 8080 localhost:5900 &
 
-# Start noVNC
-websockify --web /usr/share/novnc/ 8080 localhost:5900 &
-
-wait
+    wait
+'
